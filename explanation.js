5922817be44e8490084120f7ffbe511e6ef65f2c
ManfredHair <manfredhair@gmail.com>
2025-04-08 19:19:56 -0300

    Create explanation.js

---
+++ explanation.js
@@ -0,0 +1,16 @@
+class Item{
+    constructor(value, weight){
+        this.value = value;
+        this.weight = weight; 
+    }
+}
+
+let item1 = Item(5, 4)
+let item2 = Item(10, 8)
+let item3 = Item(3, 3)
+let item4 = Item(2, 5)
+let item5 = Item(3, 2)
+
+// objetive:
+// backpack can carry 10 weight
+// what items should we carry for the most value?
